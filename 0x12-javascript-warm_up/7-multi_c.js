e5d00ad02a95645a77fd07a05cb52e83706e7c1e
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-06-12 16:10:52 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 // a script that prints x times C is fun
-	// Where x is the first argument of the script
-	// If the first argument cant be converted to an integer, print Missing number of occurrences
-	// You must use console.log(...) to print all output
+// Where x is the first argument of the script
+// If the first argument cant be converted to an integer, print Missing number of occurrences
+// You must use console.log(...) to print all output
 
 const lang = 'C is fun';
 
-if (isNaN(process.arvg[2])){
-	console.log('Missing number of occurrences');
-}
-else {
-	for (let i = 0; i <parseInt(process.arvg[2]); i++) {
-		console.log(lang);
-	}
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(lang);
+  }
 }
