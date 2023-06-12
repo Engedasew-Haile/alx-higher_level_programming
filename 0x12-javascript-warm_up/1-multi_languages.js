b1202d71d904b564fc8b3b34c51943b1321cf27c
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-06-12 16:13:59 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,4 +4,4 @@
   //The second line: “Python is cool”
   //The third line: “JavaScript is amazing”
 
-console.log(' is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavascript is amazing');
