1a30cb3ce356c447cca9e50fa57ebd3d48107c2b
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-06-12 12:38:03 +0300

    Create 1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//a script that prints 3 lines:
+  //The first line: “C is fun”
+  //The second line: “Python is cool”
+  //The third line: “JavaScript is amazing”
+
+console.log(' is fun\nPython is cool\nJavascript is amazing');
