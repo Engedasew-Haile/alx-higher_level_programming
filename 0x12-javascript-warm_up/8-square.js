e2e0316c1447ed015f139c9757e90f928df88ffd
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-12 05:43:25 -0700

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 //a script that prints a square
-	//The first argument is the size of the square
-	//If the first argument cant be converted to an integer, print Missing size
-	//You must use the character X to print the square
-	//You must use console.log(...) to print all output
-	//You are not allowed to use var
-	//You must use a loop (while, for, etc.)
+//The first argument is the size of the square
+//If the first argument cant be converted to an integer, print Missing size
+//You must use the character X to print the square
+//You must use console.log(...) to print all output
+//You are not allowed to use var
+//You must use a loop (while, for, etc.)
 
 if (isNaN(process.arvg[2])) {
 	console.log('Missing size');
