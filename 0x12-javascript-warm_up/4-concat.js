b9e064863d75399086d03668d4f64cc9752f495b
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-06-12 12:58:56 +0300

    Create 4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+//a script that prints two arguments passed to it, in the following format: “ is ”
+  //You must use console.log(...) to print all output
+
+console.log(process.arvg[2] + ' is ' + process.arvg[3]);
