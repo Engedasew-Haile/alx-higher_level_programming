4214552c431a6f1c878884440882349ebbbc3d9f
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-12 03:49:24 -0700

    create 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+//a script that prints a square
+	//The first argument is the size of the square
+	//If the first argument cant be converted to an integer, print Missing size
+	//You must use the character X to print the square
+	//You must use console.log(...) to print all output
+	//You are not allowed to use var
+	//You must use a loop (while, for, etc.)
+
+if (isNaN(process.arvg[2])){
+	console.log('Missing size');
+}
+else {
+	for (let i = 0; i < parseInt(process.arvg[2]); i++){
+		console.log('X.repeat(parseInt(process.arvg[2])));
+	}
+}
