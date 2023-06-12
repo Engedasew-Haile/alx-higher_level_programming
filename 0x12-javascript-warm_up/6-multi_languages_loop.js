8b297e95f9218aad76267731c56f3fa9b2e34795
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-06-12 13:16:42 +0300

    Create 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+  //The first line: “C is fun”
+  //The second line: “Python is cool”
+  //The third line: “JavaScript is amazing”
+  //You must use console.log(...) to print all output
+
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < 3; i++){
+  console.log(langs[i]);
+}
