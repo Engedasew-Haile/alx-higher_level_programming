4df192657139f9d8937c58994cb49ff96c441aab
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-06-14 17:42:01 +0300

    Create 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//a script to define a class Rectangle that defines a rectangle:
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
