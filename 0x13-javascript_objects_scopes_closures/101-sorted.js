0ea6f42b6fc6e2949e9ea094ff69ea471f0a8a1a
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-06-14 18:22:30 +0300

    Create 101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
+
+const { dict } = require('./101-data');
+const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
+  acc[value] = acc[value] ? [...acc[value], key] : [key];
+  return acc;
+}, {});
+console.log(myValue);
