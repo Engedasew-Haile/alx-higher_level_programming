6db9aaf2b1936032aac5d6c788162dcd8b3e872d
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-14 07:49:13 -0700

    update

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// a script empty class Rectangle that defines a rectangle:
-  // class notation for defining your class
+// class Rectangle that defines a rectangle:
 
 class Rectangle {
 }
