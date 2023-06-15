e5b48c4557407d63f7b649f7a3185f9a63e5dfda
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-15 09:50:57 -0700

    update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 //a script that prints two arguments passed to it, in the following format: “ is ”
-  //You must use console.log(...) to print all output
+//You must use console.log(...) to print all output
 
-console.log(process.arvg[2] + ' is ' + process.arvg[3]);
+console.log(process.arvg[2] + 'is' + process.arvg[3]);
