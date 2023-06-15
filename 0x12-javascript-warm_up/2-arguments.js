c94463251bd647df10685594c140149293061799
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-15 09:39:26 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 // a script that prints a message depending of the number of arguments passed:
-//If no arguments are passed to the script, print No argument
-//If only one argument is passed to the script, print Argument found
-//Otherwise, print Arguments found
-//You must use console.log(...) to print all output
 
 if (process.argv.length === 2) {
 	  console.log('No argument');
