0a6b960352673802577359dbd5e8564a1d9a254b
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-15 09:44:31 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // a script that prints a message ... of arguments passed
 
 if (process.argv.length === 2) {
-	  console.log('No Argument');
+	  console.log('No argument');
 } else if (process.argv.length === 3) {
-	  console.log('Argument Found');
+	  console.log('Argument found');
 } else {
-	  console.log('Arguments Found');
+	  console.log('Argument found');
 }
