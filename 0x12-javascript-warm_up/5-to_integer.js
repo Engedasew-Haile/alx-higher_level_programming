b4a77f51d70a1f81a95d018e25d1202fd8386ec3
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-15 10:02:52 -0700

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-// a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
-  // If the argument can’t be converted to an integer, print “Not a number”
-  //You must use console.log(...) to print all output
+// a script that prints My number: <first argument converted in integer> if ... to an int
 
 if (isNaN(process.argv[2])){
-  console.log('Not a Number');
+  console.log('Not a number');
 } 
 else {
-  console.log('My number: ' + parseInt(process.arvg[2]));
+  console.log('My number:' + parseInt(process.arvg[2]));
 }
