6c522b6212faa2e37e21b69110d8fa34158f6857
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-18 19:36:05 -0700

    change to exe

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,16 +3,17 @@
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
+
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     database = MySQLdb.connect(port=3306,
                                host='localhost',
-                               charset='utf8',
-                               user=argv[1],
-                               passwd=argv[2],
-                               db=argv[3])
+                             # charset='utf8',
+                               username=argv[1],
+                               password=argv[2],
+                               database=argv[3])
     curs = database.cursor()
     curs.execute("SELECT * FROM states ORDER BY id ASC")
     rows = curs.fetchall()
