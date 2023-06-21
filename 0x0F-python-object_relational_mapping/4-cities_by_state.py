7dd4eb44bbf357698246583360a07633269f40d4
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-21 16:13:38 -0700

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by cities.id
 You can use only execute() once
 Results must be displayed as they are in the example below
-Your code should not be executed when imported """
+Your code should not be executed when imported"""
 import MySQLdb
 import sys
 
