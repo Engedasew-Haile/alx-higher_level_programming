afd02789a13428b4470c686c9c6f85123ca9a239
Engedasew-Haile <engedasewhaile@gmail.com>
2023-06-30 13:52:12 -0700

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """a Python script that takes in a URL, sends a request to the URL and 
-displays the value of the X-Request-Id"""
+displays the value of the X-Request-Id
+"""
+
 import sys
 import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
