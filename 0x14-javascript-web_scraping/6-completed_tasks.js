4a99fe47351c950c441a3cdf97ddec56f96777dc
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-07-26 17:03:59 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-request(process.argv[2], function (err, _res, body) {
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else {
-    const completedTasksByUsers = {};
-    body = JSON.parse(body);
-
-    for (let i = 0; i < body.length; ++i) {
-      const userId = body[i].userId;
-      const completed = body[i].completed;
-
-      if (completed && !completedTasksByUsers[userId]) {
-        completedTasksByUsers[userId] = 0;
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const todos = JSON.parse(body);
+    for (const todo of todos) {
+      if (todo.completed === true) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId]++;
+        }
       }
-
-      if (completed) ++completedTasksByUsers[userId];
     }
-
-    console.log(completedTasksByUsers);
+    console.log(completed);
+  } else {
+    console.log('An error occurred. Status code: ' + response.statusCode);
   }
 });
