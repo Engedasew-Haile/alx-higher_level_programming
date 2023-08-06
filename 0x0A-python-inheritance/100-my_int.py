a8531150a712eed9ba0195138ef56b9ef9cdb36d
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-08-06 14:54:43 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python3
+"""
+=============================
+a class MyInt that inherits from int:
+    MyInt is a rebel. MyInt has == and != operators inverted
+=============================
+"""
+
+
 class MyInt(int):
-    """creates a class MyInt"""
+    """class that inherits from int"""
+
+    def __init__(self, my_int):
+        """for initialize a value my_int"""
+
+        self.my_int = my_int
+
+    @property
+    def my_int(self):
+        return self.__my_int
+
+    @my_int.setter
+    def my_int(self, my_int):
+        if type(my_int) is not int:
+            raise TypeError("my_int must be an integer")
+        else:
+            self.__my_int = my_int
+
     def __eq__(self, other):
-        """invert == to !="""
-        return False
+        """equal method"""
+
+        if self.my_int == other:
+            return False
+        else:
+            return True
 
     def __ne__(self, other):
-        """invert != to =="""
-        return True
+            if self.my_int != other:
+                return False
+            else:
+                return True
